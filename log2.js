c60793edcd5fbbf2db7e4f5bd38ac442900056ce
abdeljalil47 <abdeljalilsebbar1@gmail.com>
2023-11-03 10:25:55 +0100

    first commit

---
+++ log2.js
@@ -0,0 +1,28 @@
+/* username label */
+function showLabel() {
+    const input = document.getElementById('username');
+    const label = document.getElementById('user');
+    const input1 = document.getElementById('password');
+    const label1 = document.getElementById('pass');
+    if (input.value === ""){
+        label.style.visibility = "hidden";
+    } else {
+        label.style.visibility = "visible";
+    }
+    if (input1.value === ""){
+        label1.style.visibility = "hidden";
+    } else {
+        label1.style.visibility = "visible";
+    }
+}
+document.getElementById('button').addEventListener('click',function(){
+    const input = document.getElementById('username');
+    const input1 = document.getElementById('password');
+    const line = 8;
+
+    if (input.value.length < line && input1.value.length < line){
+        alert('You forgot somethings !')
+    }else{
+        alert('Welcome');
+    }
+})
